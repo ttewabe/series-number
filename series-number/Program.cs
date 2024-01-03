f7d9720c44d078ac69d0c26cdd85eb794b041a62
tomta <tomta@TEAM>
2024-01-02 17:08:06 -0800

    Add project files.

---
+++ series-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i=5; i>=1; i--)
+            {
+                int x = i * 2;
+                int y = i * 3;
+
+                for(int j = 1; j<x; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
